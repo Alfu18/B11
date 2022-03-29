8e78673dffc165eca06e7186e9e835f117081f3b
Admin1 <Admin1@Admin>
2022-03-30 01:00:41 +0530

    1st commit

---
+++ B11 Java/src/Day8/Scanner.java
@@ -0,0 +1,30 @@
+package Day8;
+
+//import;
+
+public class Scanner {
+
+	private String next;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner sc=new Scanner();
+	System.out.println("what is your name");
+	String name=sc.next;
+	System.out.println("dice");
+	
+	System.out.println("what is your age");
+	int age=sc.nextInt();
+	System.out.println("15");
+	
+	System.out.println("Thank you,"+ name+",you are"+age+"");
+			
+	
+	}
+
+	private int nextInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
